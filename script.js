ce862c8a714c5f8a760a4c944400be8a8a77a139
Fitratul Aini <fitratulllni@gmail.com>
2025-05-29 12:29:50 +0700

    add script.js to fetch and display mahasiswa data from coba.json

---
+++ script.js
@@ -0,0 +1,18 @@
+// let mahasiswa = {
+    // nama : "fitratul Aini",
+    // nim : "2217020008",
+    // email : "fitratul@gmail.com"
+// }
+// 
+// console.log(JSON.stringify(mahasiswa));
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        let mahasiswa = JSON.parse(this.responseText);
+        console.log(mahasiswa);
+    }
+}
+
+xhr.open('GET', 'coba.json', true);
+xhr.send();
