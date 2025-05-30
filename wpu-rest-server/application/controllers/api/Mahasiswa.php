8cf653462ed2272cf71115c6adb92969d238bdbb
Fitratul Aini <fitratulllni@gmail.com>
2025-05-30 21:13:05 +0700

    menambahkan metode hapus, buat, dan perbarui di pengontrol dan model

--- wpu-rest-server/application/controllers/api/Mahasiswa.php
+++ wpu-rest-server/application/controllers/api/Mahasiswa.php
@@ -41,6 +41,86 @@ class Mahasiswa extends REST_Controller
 
     public function index_delete()
     {
-        
+        $id = $this->delete('id');
+
+        if ($id === null){
+            $this->response([
+                'status' => false,
+                'message' => 'provide an id!'
+            ], REST_Controller::HTTP_BAD_REQUEST); 
+        } else {
+            if ($this->mahasiswa->deleteMahasiswa($id) > 0 ) {
+                // OK
+                $this->response([
+                    'status' => true,
+                    'id' => $id,
+                    'message' => 'deleted.'
+                ], REST_Controller::HTTP_NO_CONTENT); 
+                // Ganti dari NO_CONTENT ke OK
+            } else {
+                // ID not found
+                $this->response([
+                    'status' => false,
+                    'message' => 'id not found!'
+                ], REST_Controller::HTTP_BAD_REQUEST); 
+            }
+        }
     }
+
+
+    public function index_post()
+    {
+        $data = [
+            'nrp' => $this->post('nrp'),
+            'nama' => $this->post('nama'),
+            'email' => $this->post('email'),
+            'jurusan' => $this->post('jurusan'),
+            
+        ];
+
+
+        if ($this->mahasiswa->createMahasiswa($data) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'new mahasiswa has been created.'
+            ], REST_Controller::HTTP_CREATED); 
+
+        }  else {
+                $this->response([
+                    'status' => false,
+                    'message' => 'failed to create new data!'
+                ], REST_Controller::HTTP_BAD_REQUEST); 
+            }
+
+    }
+
+    public function index_put ()
+    {
+        $id = $this->put('id');
+        $data = [
+            'nrp' => $this->put('nrp'),
+            'nama' => $this->put('nama'),
+            'email' => $this->put('email'),
+            'jurusan' => $this->put('jurusan'),
+            
+        ];
+
+        if ($this->mahasiswa->updateMahasiswa($data, $id) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'data mahasiswa has been updated.'
+            ], REST_Controller::HTTP_NO_CONTENT); 
+
+        }  else {
+                $this->response([
+                    'status' => false,
+                    'message' => 'failed to update data!'
+                ], REST_Controller::HTTP_BAD_REQUEST); 
+            }
+
+
+    }
+
 }
+
+
--- wpu-rest-server/application/models/Mahasiswa_model.php
+++ wpu-rest-server/application/models/Mahasiswa_model.php
@@ -12,4 +12,22 @@ class Mahasiswa_model extends CI_Model
         }
 
     }
+
+    public function deleteMahasiswa($id) 
+    {
+        $this->db->delete('mahasiswa',['id' => $id]);
+        return $this->db->affected_rows();
+    }
+
+    public function createMahasiswa($data)
+    {
+        $this->db->insert('mahasiswa', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function updateMahasiswa($data, $id)
+    {
+        $this->db->update('mahasiswa',$data, ['id' => $id]);
+        return $this->db->affected_rows();
+    }
 }
