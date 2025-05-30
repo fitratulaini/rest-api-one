0b2ba0dfd9d009476c23ed9e8dc7a56288355131
Fitratul Aini <fitratulllni@gmail.com>
2025-05-30 14:04:21 +0700

    rest server

---
+++ wpu-rest-server1/application/controllers/api/Mahasiswa.php
@@ -0,0 +1,24 @@
+<?php
+
+use Restserver\Libraries\REST_Controller;
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Mahasiswa extends REST_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Mahasiswa_model', 'mahasiswa');
+    }
+
+    public function index_get()
+    {
+        $mahasiswa = $this->mahasiswa->getMahasiswa();
+        
+        
+    }
+}
